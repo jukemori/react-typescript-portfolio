164f9ff6af7ffea7f504e84b06c7440d23e58a21
Jun Ukemori <jukemori@gmail.com>
2023-08-07 17:29:27 +0900

    set testimonial components

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,60 +1,32 @@
 import "./testimonials.css";
-import { Data } from "./Data";
+
 import Avatar from "../../assets/xzaiver.jpg";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-
-// import "./styles.css";
-
-// import required modules
-import { Pagination } from "swiper/modules";
 
 function Testimonials() {
   return (
     <section className="testimonial container section">
       <h2 className="section__title">Testimonial</h2>
-      <span className="section__subtitle">Testimonial</span>
+      <span className="section__subtitle">Very reliable </span>
 
-      <Swiper
-        className="testimonial__container"
-        loop={true}
-        grabCursor={true}
-        spaceBetween={24}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          576: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 48,
-          },
-        }}
-        modules={[Pagination]}
-      >
-        {Data.map(({ id, image, title, description }) => {
-          return (
-            <SwiperSlide className="testimonial__card" key={id}>
-              <img src={image} alt="" className="testimonial__img" />
-
-              <h3 className="testimonial__name">{title}</h3>
-              <p className="testimonial__description">{description}</p>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-
-      <div className="testimonial__container">
+      <div className="testimonial__container container">
         <div className="testimonial__card">
-          <img src={Avatar} alt="" className="testimonal__img" />
-          <h3 className="testimonial__name">X'Zaiver Wolfinbarger</h3>
-          <p className="testimonial__description">He's the goat</p>
+          <h3 className="testimonial__title">
+            <i className="bx bxs-quote-left"></i> He's the goat
+          </h3>
+          <div className="testimonial__box">
+            <img src={Avatar} alt="" className="testimonial__img" />
+            <div className="testimonial__description">
+              <div>
+                <i className="bx bxs-star testimonial__icon"></i>
+                <i className="bx bxs-star testimonial__icon"></i>
+                <i className="bx bxs-star testimonial__icon"></i>
+                <i className="bx bxs-star testimonial__icon"></i>
+                <i className="bx bxs-star testimonial__icon"></i>
+              </div>
+              <p className="testimonial__name">X'Zaiver Wolfinbarger</p>
+              <p className="testimonial__subtitle">Le Wagon</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
