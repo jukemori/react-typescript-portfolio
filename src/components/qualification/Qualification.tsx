9bb834b7a9b84d632f7016e2de73bcd69e37da19
Jun Ukemori <jukemori@gmail.com>
2023-08-07 20:42:17 +0900

    modified header

--- src/components/qualification/Qualification.tsx
+++ src/components/qualification/Qualification.tsx
@@ -9,7 +9,7 @@ function Qualification() {
   };
 
   return (
-    <section className="qualification section" id="qualification">
+    <section className="qualification section" id="background">
       <h2 className="section__title">Background</h2>
       <span className="section__subtitle">My personal journey</span>
       <div className="qualification__container container">
